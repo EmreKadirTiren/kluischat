8d1104045c54326290cbf6ba6c1dfac39ec053fc
Emre Kadir Tiren <77925614+EmreKadirTiren@users.noreply.github.com>
2024-07-12 19:54:16 +0000

    Made a part of the message page

---
+++ client/src/pages/chat/messages.js
@@ -0,0 +1,66 @@
+import styles from './styles.module.css';
+import { useState, useEffect } from 'react';
+
+const Messages = ({ socket }) => {
+  const [messagesRecieved, setMessagesReceived] = useState([]);
+
+  // Runs whenever a socket event is recieved from the server
+  useEffect(() => {
+    // Listening for welcome messages
+    socket.on('welcome_message', (data) => {
+      console.log('Welcome message:', data);
+      setMessagesReceived((state) => [
+        ...state,
+        {
+          message: data.message,
+          username: data.username,
+          __createdtime__: data.__createdtime__,
+        },
+      ]);
+    });
+
+    // Listening for regular chat messages
+    socket.on('receive_message', (data) => {
+      console.log(data);
+      setMessagesReceived((state) => [
+        ...state,
+        {
+          message: data.message,
+          username: data.username,
+          __createdtime__: data.__createdtime__,
+        },
+      ]);
+    });
+
+    // Remove event listeners on component unmount
+    return () => {
+      socket.off('receive_message');
+      socket.off('welcome_message');
+    };
+  }, [socket]);
+
+  // dd/mm/yyyy, hh:mm:ss
+  function formatDateFromTimestamp(timestamp) {
+    const date = new Date(timestamp);
+    return date.toLocaleString();
+  }
+
+  return (
+    <div className={styles.messagesColumn}>
+      {messagesRecieved.map((msg, i) => (
+        <div className={styles.message} key={i}>
+          <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+            <span className={styles.msgMeta}>{msg.username}</span>
+            <span className={styles.msgMeta}>
+              {formatDateFromTimestamp(msg.__createdtime__)}
+            </span>
+          </div>
+          <p className={styles.msgText}>{msg.message}</p>
+          <br />
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default Messages;

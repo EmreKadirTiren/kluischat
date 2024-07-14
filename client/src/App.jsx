2309221e944b158916cb9059f0c740f6f8dbd362
Emre Kadir Tiren <77925614+EmreKadirTiren@users.noreply.github.com>
2024-07-14 19:40:47 +0000

    Enhancements to ChannelListContainer and Addition of Assets Folder
    This commit further refines the ChannelListContainer component and introduces an assets folder to the project structure. The enhancements to ChannelListContainer aim to improve the overall user interface by integrating the SideBar and CompanyHeader components more seamlessly. Additionally, the creation of an assets folder allows for better organization and management of static resources such as images, icons, and stylesheets.
    
    Key Changes:
    - Modified the ChannelListContainer component to include the SideBar and CompanyHeader components, enhancing the application's navigation and branding.
    - Added an assets folder to the project directory, containing initial static resources required for the UI. This folder will serve as a central repository for all visual and static elements used across the application.
    
    These updates contribute to a more organized codebase and a visually appealing user interface for the Kluizenaren Chat application.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,6 +4,9 @@ import { Chat } from 'stream-chat-react'; // import Chat from stream-chat-react
 import Cookies from 'universal-cookie'; // import Cookies from universal-cookies
 
 import { ChannelContainer, ChannelListContainer } from './components'; // import ChannelContainer and ChannelListContainer from components folder
+import './App.css'; // import App.css file
+
+
 const apiKey = 'usexrsqhpvtk' // this is key not the secret this is public key and can be published.
 
 const client = StreamChat.getInstance(apiKey); // create a new instance of StreamChat with the apiKey
